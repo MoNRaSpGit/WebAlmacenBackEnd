59385f7dba792921df3109bec51216470585d2aa
MoNRaSpGit <ju4nrsuarez@gmail.com>
2025-10-25 03:38:33 -0300

    dsfds

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.get("/favicon.ico", (req, res) => res.status(204).end());
+
 
 // Ruta de prueba para verificar conexión
 app.get("/", async (req, res) => {
